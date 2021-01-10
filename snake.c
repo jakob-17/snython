61813828b36152e832094021c8a084df815b0deb
Jakob <horvath.j17@gmail.com>
2021-01-10 11:48:52 -0700

    basic texture and directional input added

--- snake.c
+++ snake.c
@@ -12,6 +12,8 @@ int main()
 					   SDL_WINDOWPOS_CENTERED,
 					   SDL_WINDOWPOS_CENTERED,
 					   1000, 1000, 0);
+
+	SDL_SetWindowTitle(win, "Snython");
 	
 	// triggers program that controls graphics hardware and sets flags
 	Uint32 render_flags = SDL_RENDERER_ACCELERATED;
@@ -52,7 +54,83 @@ int main()
 	int speed = 300;
 
 	// animation loop
+	while (!close)
+	{
+		SDL_Event event;
+
+		// events management
+		while (SDL_PollEvent(&event))
+		{
+			// turn off compiler warnings of unused switch cases
+			#pragma GCC diagnostic push
+			#pragma GCC diagnostic ignored "-Wswitch"
+			switch (event.type)
+			{
+				case SDL_QUIT:
+					// handling of close button
+					close = 1;
+					break;
+
+				case SDL_KEYDOWN:
+					// keyboard API for key pressed
+					switch (event.key.keysym.scancode)
+					{
+						case SDL_SCANCODE_W:
+						case SDL_SCANCODE_UP:
+							dest.y -= speed / 30;
+							break;
+						case SDL_SCANCODE_A:
+						case SDL_SCANCODE_LEFT:
+							dest.x -= speed / 30;
+							break;
+						case SDL_SCANCODE_S:
+						case SDL_SCANCODE_DOWN:
+							dest.y += speed / 30;
+							break;
+						case SDL_SCANCODE_D:
+						case SDL_SCANCODE_RIGHT:
+							dest.x += speed / 30;
+							break;
+					}	
+			}
+			
+			#pragma GCC diagnostic pop	
+		}
 	
+
+
+		// right boundary
+		if (dest.x + dest.w > 1000)
+			dest.x = 1000 - dest.w;
+		
+		// left boundary
+		if (dest.x < 0)
+			dest.x = 0;
+
+		// bottom boundary
+		if (dest.y + dest.h > 1000)
+			dest.y = 1000 - dest.h;
+
+		// upper boundary
+		if (dest.y < 0)
+			dest.y = 0;
+
+		// clears the screen
+		SDL_RenderClear(rend);
+		SDL_RenderCopy(rend, tex, NULL, &dest);
+
+		// triggers the double buffers for multiple rendering
+		SDL_RenderPresent(rend);
+
+		// calculates to 60 fps
+		SDL_Delay(1000 / 60);
+	}
+
+	SDL_DestroyTexture(tex);
+
+	SDL_DestroyRenderer(rend);
+
+	SDL_DestroyWindow(win);	
 
 	return 0;
 }
